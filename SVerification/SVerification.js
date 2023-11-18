2800be8bc66c54fef529606f994f41ea35c5d37b
YasminaDada <yasminadada18@gmail.com>
2023-11-18 12:40:58 +0100

    Last changes

---
+++ SVerification/SVerification.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const {DmModification}= require('../db');
+
+
+
+router.get('/verify-modification/:idInscription', async (req, res) => {
+  const { idInscription } = req.params;
+
+  try {
+    const existingDmModification = await DmModification.findOne({ inscription: idInscription });
+
+    if (existingDmModification) {
+      res.json({ accepted: existingDmModification.accepted });
+    } else {
+      res.json({ accepted: false });
+    }
+  } catch (error) {
+    console.error('Error verifying modification:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
---
+++ db.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/Edugate', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const etudiantSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    cne: String,
+    fullname: String,
+  });
+  
+  const gestionnaireSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+  });
+
+  const inscriptionSchema = new mongoose.Schema({
+    idInscription: String,
+    cne: String,
+    filiere: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Filiere',
+    },
+    dateInscription: Date,
+  });
+  
+  const filiereSchema = new mongoose.Schema({
+    idFiliere: String,
+    nom: String,
+    nbEtu: Number,
+  });
+  
+  const dmModificationSchema = new mongoose.Schema({
+    idModification: String,
+    inscription: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Inscription',
+    },
+    actionDate: Date,
+    accepted: Boolean,
+  });
+  
+  const Inscription = mongoose.model('inscription', inscriptionSchema);
+  const Filiere = mongoose.model('filiere', filiereSchema);
+  const DmModification = mongoose.model('dmModification', dmModificationSchema);  
+  const Gestionnaire = mongoose.model('gestionnaire', gestionnaireSchema);
+  const Etudiant = mongoose.model('etudiant', etudiantSchema);
+  
+  module.exports = {Gestionnaire,Etudiant,Inscription, Filiere, DmModification,mongoose};
